3ceca1776da5d35ea43ae6dfe37ff03dbceb9af3
beardhatcode <mail.garonn@gmail.com>
2018-01-11 15:05:41 +0100

    Runtime statistics per request
    dodona.controllers.ExercisesController.index.html.db_runtime:8.946325999999997|ms
    dodona.controllers.ExercisesController.index.html.view_runtime:125.9502279998224|ms
    dodona.controllers.ExercisesController.index.html.total_runtime:140.397139|ms

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -1,2 +1,12 @@
 StatsD.backend = StatsD::Instrument::Backends::UDPBackend.new("localhost:8125", :statsd)
 StatsD.prefix  = "dodona"
+
+
+ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |name, start, ending, transaction_id, payload|
+    format = payload[:format] || "all" 
+    format = "all" if format == "*/*" 
+    key = "controllers.#{payload[:controller]}.#{payload[:action]}.#{format}." 
+    StatsD.measure(key+"db_runtime", payload[:db_runtime], sample_rate: 1.0)
+    StatsD.measure(key+"view_runtime", payload[:view_runtime], sample_rate: 1.0)
+    StatsD.measure(key+"total_runtime", 1000.0 * (ending - start), sample_rate: 1.0) # time in ms
+  end
